f2caa88a7ebc7279aed26e067611ecba00bd1a02
madejean <madejean007@hotmail.fr>
2018-02-01 14:02:33 -0800

    adding links

---
+++ 0x04-react_router/0-links/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Header = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Holberton">Holberton</Link></li>
+        <li><Link to="/School">School</Link></li>
+      </ul>
+    </div>
+  </Router>
+)
+export default Header;
